a243d97daa2732d2efbdaa293834d7b868a22637
Timjini <hatim.jini@gmail.com>
2022-01-28 18:08:08 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Request.create(description: "Test request 001", address: "Paris", category: 1, status: 1)
