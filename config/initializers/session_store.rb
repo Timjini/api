2c781ea6e6d0046878b67d4052d45d709aa0a991
Timjini <hatim.jini@gmail.com>
2022-02-08 11:10:44 +0300

    update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'mighty-tor-35081.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: '_api', domain: 'mighty-tor-35081.herokuapp.com'
   else
     Rails.application.config.session_store :cookie_store, key: '_api'
   end
