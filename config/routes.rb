b90b8539aa23eb8729c4172a833cca9a760aca3f
Timjini <hatim.jini@gmail.com>
2022-02-08 11:08:39 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,11 @@ Rails.application.routes.draw do
 resources :sessions, only: [:create, :show]
 resources :users, only: [:create, :show, :index] 
 resources :registrations, only: [:create]   
-delete  :logout, to:"sessions#logout"
+delete  :logout, to:"sessions#logged_out"
 get :looged_in, to: "sessions#logged_in"   
-#post '/users',         to: 'users#create'
-#get '/users/:user_id', to: 'users#show'
-#get '/users',          to: 'users#index'
-#post '/login',    to: 'sessions#create'
-#post '/logout',   to: 'sessions#destroy'
-#get '/logged_in', to: 'sessions#is_logged_in?'
+
+    end
+
+  end 
 end
 
-    end 
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
